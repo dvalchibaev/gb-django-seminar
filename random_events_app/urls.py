4136b52422b99184c6f4608d1802ca729c6f708c
dvalchibaev <alchibaevdv@gmail.com>
2023-09-01 14:27:06 +0400

    lesson02_practice_coin_toss

--- random_events_app/urls.py
+++ random_events_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("heads_or_tails/", views.heads_or_tails, name="toss_a_coin"),
     path("roll_d6/", views.roll_d6, name="roll_d6"),
-    path("random_100/", views.random100, name="random_100")
+    path("random_100/", views.random100, name="random_100"),
+    path("tosses/", views.get_last_tosses, name="last_tosses"),
 ]
