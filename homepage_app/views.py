7801e37fc3b4a24f8217b21550b4109f6958a090
dvalchibaev <alchibaevdv@gmail.com>
2023-09-04 23:56:42 +0400

    seminar

--- homepage_app/views.py
+++ homepage_app/views.py
@@ -8,24 +8,10 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-    try:
-        with open("./homepage_app/static/index.html", "r") as page:
-            html_response = page.read()
-    except Exception as e:
-        logger.exception(f'Error in home page: {e}')
-        return HttpResponse("500 Internal server error")
-    else:
-        logger.info("Home page accessed")
-        return HttpResponse(html_response)
+    template = "homepage_app/index.html"
+    return render(request, template)
 
 
 def about(request):
-    try:
-        with open("./homepage_app/static/about.html", "r") as page:
-            html_response = page.read()
-    except Exception as e:
-        logger.exception(f'Error in about page: {e}')
-        return HttpResponse("500 Internal server error")
-    else:
-        logger.info("About page accessed")
-        return HttpResponse(html_response)
+    template = "homepage_app/about.html"
+    return render(request, template)
