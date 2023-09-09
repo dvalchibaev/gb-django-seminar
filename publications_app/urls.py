9697d2459a34a969b0e6b09ea650197720dc1ad8
dvalchibaev <alchibaevdv@gmail.com>
2023-09-09 19:01:00 +0400

    refactor publications_app lesson02 initial

--- publications_app/urls.py
+++ publications_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("authors/", views.authors, name="authors"),
     path("articles/", views.articles, name="articles"),
-    path("comments/", views.comments, name="comments")
+    path("comments/", views.comments, name="comments"),
+    path("articles_by/<int:author_id>/", views.PubView.as_view(), name="publications_by_author")
 ]
