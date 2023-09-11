fcd2526dbcf9af1218daa4707e09c3b4b5541f48
dvalchibaev <alchibaevdv@gmail.com>
2023-09-11 13:38:59 +0400

    homework 03

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("clients/", views.clients, name="clients"),
     path("orders/", views.orders, name="orders"),
     path("items/", views.items, name="items"),
+    path("client_orders/<int:client_id>/", views.ClientOrders.as_view(), name="client_orders"),
+    path("client_orders/<int:client_id>/<str:date_range>/", views.ClientOrders.as_view(), name="client_orders"),
+    path("create_orders/<str:date>/", views.create_fake_orders, name="create_fake_orders"),
 ]
